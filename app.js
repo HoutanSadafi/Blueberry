ce9ace9c998bf39e4adf83f6d4462ca59d9d5cf8
HoutanSadafi <certop18@hotmail.com>
2012-07-11 02:21:25 -0400

    Moved all public assets under a public folder. Fixed case-sensitivity issues with paths.

--- app.js
+++ app.js
@@ -70,8 +70,7 @@ app.configure(function(){
   app.set('view options', { layout: false, pretty: true });
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use('/Styles', express.static(__dirname + '/Styles'));
-  app.use('/Images', express.static(__dirname + '/Images'));
+  app.use('/public', express.static(__dirname + '/public'));
   app.use(app.router);
   app.use(function(err, req, res, next){
     res.status(500);
