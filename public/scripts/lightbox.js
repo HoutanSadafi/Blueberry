10ef72d791b6adc55c30fa1cc012346b4b454ab3
HoutanSadafi <certop18@hotmail.com>
2012-12-03 21:52:53 -0500

    Moved lightbox out of jquery document ready function into its own object. Hovering over images changes cursor to pointer

--- public/scripts/lightbox.js
+++ public/scripts/lightbox.js
@@ -1,43 +1,7 @@
 (function($){
 
     $(document).ready(function($) {
-         
-        $('img.lb').click(function(e) {
-            e.preventDefault();
-            var image_href = $(this).attr('lb-url');
-            if (!image_href){
-            	var image_href = $(this).attr('src');		
-            }
-             
-            var exists = $('#lightbox-overlay').length; 
-            if (!exists) {
-            	var markup = '<div id="lightbox"></div><div id="lightbox-overlay"></div>';     
-                $('body').append(markup);
-            } else {
-            	$('#lightbox').html('');	
-            }
-
-            var $lightbox = $('#lightbox');
-            var $overlay = $('#lightbox-overlay');
-            
-            $lightbox.hide();
-            var img = document.createElement('img');
-            img.setAttribute('src', image_href);
-            img.setAttribute('class', 'polaroid');
-            img.onload = function(){
-                $overlay.show();
-                $lightbox.center();
-                $lightbox.fadeIn();
-            };
-
-            $lightbox.append(img);
-        });
-         
-        $('#lightbox-overlay').live('click', function() {
-        	$(this).hide();
-        	$('#lightbox').hide();
-        });
-     
+        lightbox.init();
     });
 
     $.fn.center = function () {
@@ -47,8 +11,48 @@
         return this;
     }
 
-    $(window).bind('resize', function() {
-    	$("#lightbox").center();
-    });
+    var lightbox = {
+        init : function(){
+            $('img.lb').click(function(e) {
+                e.preventDefault();
+                var image_href = $(this).attr('lb-url');
+                if (!image_href){
+                    var image_href = $(this).attr('src');       
+                }
+                 
+                var exists = $('#lightbox-overlay').length; 
+                if (!exists) {
+                    var markup = '<div id="lightbox"></div><div id="lightbox-overlay"></div>';     
+                    $('body').append(markup);
+                } else {
+                    $('#lightbox').html('');    
+                }
+
+                var $lightbox = $('#lightbox');
+                var $overlay = $('#lightbox-overlay');
+                
+                $lightbox.hide();
+                var img = document.createElement('img');
+                img.setAttribute('src', image_href);
+                img.setAttribute('class', 'polaroid');
+                img.onload = function(){
+                    $overlay.show();
+                    $lightbox.center();
+                    $lightbox.fadeIn();
+                };
+
+                $lightbox.append(img);
+            });
+         
+            $('#lightbox-overlay').live('click', function() {
+                $(this).hide();
+                $('#lightbox').hide();
+            }); 
+
+            $(window).bind('resize', function() {
+                $("#lightbox").center();
+            }); 
+        }
+    };
 
 })(jQuery)
